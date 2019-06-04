b8982f85f3f06db01fd04d47db6f4020b608e63a
tati-tati <49431500+tati-tati@users.noreply.github.com>
2019-06-04 10:52:09 +0300

    Update Dock.java

--- src/Dock.java
+++ src/Dock.java
@@ -1,4 +1,4 @@
-public class Dock implements Runnable {
+public class Dock extends Thread {
     private final int VELOCITY_IN_SECOND = 10;
     private Ship.Type type;
     private Channel channel;
@@ -22,12 +22,12 @@ public class Dock implements Runnable {
 
     @Override
     public void run() {
+      System.out.println("New thread is invoked : " + Thread.currentThread().getName());
         while (true){
             Ship ship = channel.shipIsLeaving(getType());
             if (ship != null){
                 System.out.println(toString() + " get " + ship.toString() +
-                        " thread : " +
-                        Thread.currentThread().getName());
+                        " thread : " + Thread.currentThread().getName());
                 try {
                     Thread.sleep(ship.getCapacity() / VELOCITY_IN_SECOND * 1000);
                     ship.setCurrFilling(ship.getCapacity());
